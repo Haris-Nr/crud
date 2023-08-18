cc636b34b644eef24150d53bb38feb1478348b28
Haris <121281257+Haris-Nr@users.noreply.github.com>
2023-08-18 14:28:08 +0500

    add

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const app = require('express')()
+const express = require('express')
+const path = require('path')
+
+const app = express()
 
 
 
+// app.get('/query',(req,res)=>{
+//     console.log(req.query)
+//     res.send(`query throung url ${req.query.name}`)
+// })
 
+// app.get('/about',(req,res)=>{
+//     res.send("<h1>helo</h1>")
+// })
 
-app.get('/query',(req,res)=>{
-    console.log(req.query)
-    res.send(`query throung url ${req.query.name}`)
-})
-
-app.get('/about',(req,res)=>{
-    res.send("<h1>helo</h1>")
-})
-
-
+ const publicPath = path.join(__dirname,'public');
+ app.use(express.static(publicPath))
 
 
 app.listen(4000,()=>{
