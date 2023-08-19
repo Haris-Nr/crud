9299a38dd7521d62fe48c433b233562ddf71078a
Haris <121281257+Haris-Nr@users.noreply.github.com>
2023-08-19 11:01:32 +0500

    add

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const app = express()
 
-
+app.set("view engine","ejs");
 
 // app.get('/query',(req,res)=>{
 //     console.log(req.query)
@@ -15,7 +15,30 @@ const app = express()
 // })
 
  const publicPath = path.join(__dirname,'public');
- app.use(express.static(publicPath))
+//  app.use(express.static(publicPath))
+
+
+// app.get('',(req,res)=>{
+//     res.sendFile(`${publicPath}/home.html`)
+// })
+
+// app.get('/about',(req,res)=>{
+//     res.sendFile(`${publicPath}/about.html`)
+// })
+
+// app.get('*',(req,res)=>{
+//     res.sendFile(`${publicPath}/404.html`)
+// })
+
+app.get('/profile',(req,res)=>{
+    const user={
+        name:'haris',
+        age:25,
+        skills:["React","Nodejs","MongoDB","expressjs"]
+        }
+    res.render('profile',{user})
+})
+
 
 
 app.listen(4000,()=>{
